f74271814c3db2e5699a407e0eca2268608978f6
tjheckler <timothyheckler@gmail.com>
2018-04-17 16:00:08 -0500

    added comment

---
+++ ch02/Date.java
@@ -0,0 +1,22 @@
+public class Date
+{
+    public static void main(String[] args)
+    {
+        String day = "Tuesday ";
+        String month = "April ";
+        int date = 16;
+        int year = 2018;
+        System.out.println("American Format:");
+        System.out.print(day);
+        System.out.print(month);
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.println(year);
+        System.out.println("European Format:");
+        System.out.print(day);
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.println(year);
+    }
+}
