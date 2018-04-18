bffa7de5f11d98c0106ac97aaf3f1aef58595937
tjheckler <timothyheckler@gmail.com>
2018-04-18 15:57:49 -0500

    4-a

--- ch04/SimpleMethods.java
+++ ch04/SimpleMethods.java
@@ -3,9 +3,16 @@ public class SimpleMethods
     public void main (String[] args)
     {
         printCount(5);
+        printSum(4,6);
+        printSum(7,2);
     }
     public static void printCount(int count)
     {
         System.out.println("The count is: " + count);
+    }
+
+    public static void printSum(int x, int y)
+    {
+        System.out.println(x+y);
     }
 }
