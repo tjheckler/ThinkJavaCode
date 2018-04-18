4aa375ab73cedda04d68a3c7f72cc0a35ab699b4
tjheckler <timothyheckler@gmail.com>
2018-04-18 15:41:24 -0500

    4-a

---
+++ ch04/SimpleMethods.java
@@ -0,0 +1,11 @@
+public class SimpleMethods
+{
+    public void main (String[] args)
+    {
+        printCount(5);
+    }
+    public static void printCount(int count)
+    {
+        System.out.println("The count is: " + count);
+    }
+}
