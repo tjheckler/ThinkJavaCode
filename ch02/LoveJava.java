83ff3db471d875bf1658437c775309a0e029c18f
tjheckler <timothyheckler@gmail.com>
2018-04-17 20:14:03 -0500

    added comment

---
+++ ch02/LoveJava.java
@@ -0,0 +1,11 @@
+public class LoveJava
+{
+    public static void main (String[] args)
+    {
+        System.out.println("I love Java!");
+        String firstWord = "I ";
+        String secondWord = "love ";
+        String thirdWord = "Java!";
+        System.out.println(firstWord + secondWord + thirdWord);
+    }
+}
