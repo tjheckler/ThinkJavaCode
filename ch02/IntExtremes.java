a124b0cfc2b838cd20476ef986e28c141ad303ee
tjheckler <timothyheckler@gmail.com>
2018-04-17 19:52:21 -0500

    added comment

---
+++ ch02/IntExtremes.java
@@ -0,0 +1,18 @@
+public class IntExtremes
+{
+    public static void main(String[] args)
+    {
+        //Increas max positive int
+        int positiveInt = 2147483647;
+        System.out.println(positiveInt);
+        positiveInt++;
+        System.out.println(positiveInt);
+        int negativeInt = -2147483648;
+
+        //Decrease max negative int
+        System.out.println(negativeInt);
+        negativeInt--;
+        System.out.println(negativeInt);
+
+    }
+}
