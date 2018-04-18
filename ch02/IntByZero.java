30dc09eed3fd3612d7e5d7593a18ddae79eb1c76
tjheckler <timothyheckler@gmail.com>
2018-04-17 20:01:28 -0500

    Fixed Typo in comment

---
+++ ch02/IntByZero.java
@@ -0,0 +1,12 @@
+public class IntByZero
+{
+    public static void main (String[] args)
+    {
+
+        //Divide by zero error demo
+        int first = 42;
+        int second = 0;
+        int result = first / second;
+        System.out.println(result);
+    }
+}
