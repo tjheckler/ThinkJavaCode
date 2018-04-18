0c02de473cf12975e8f9893ed178faba3711eb27
tjheckler <timothyheckler@gmail.com>
2018-04-17 20:07:55 -0500

    added comment

---
+++ ch02/DoubleByZero.java
@@ -0,0 +1,12 @@
+public class DoubleByZero
+{
+    public static void main (String[] args)
+    {
+
+        //Divide by zero error demo
+        double first = 42;
+        double second = 0;
+        double result = first / second;
+        System.out.println(result);
+    }
+}
