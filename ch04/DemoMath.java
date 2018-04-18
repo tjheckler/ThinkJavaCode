dd370fb5e529ea69339130203386ffa02fa4314f
tjheckler <timothyheckler@gmail.com>
2018-04-18 16:23:50 -0500

    4e

---
+++ ch04/DemoMath.java
@@ -0,0 +1,13 @@
+public class DemoMath
+{
+    public static void main(String[] args)
+    {
+        int abs = Math.abs(-4);
+        System.out.println(abs);
+        int max = Math.max(7,10);
+        System.out.println(max);
+        double pow = Math.pow(5.0, 6.6);
+        System.out.println(pow);
+        System.out.println(Math.PI);
+    }
+}
