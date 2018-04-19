c46674b4aa559883f33e46320cfec3c9a1073f05
tjheckler <timothyheckler@gmail.com>
2018-04-19 11:37:33 -0500

    5-es-1

---
+++ ch05/Logic.java
@@ -0,0 +1,15 @@
+public class Logic
+{
+    public static void main(String[]args)
+    {
+        boolean yes = true;
+        boolean no = false;
+        System.out.println("Both YesYes True: "+ (yes && yes));
+        System.out.println("Both YesNo True: "+(yes && no));
+        System.out.println("Either YesYes True: "+(yes || yes));
+        System.out.println("Either YesNo True: "+ (yes|| no));
+        System.out.println("Either NoNo True: "+(no | no));
+        System.out.println("Origiona Yes Value: "+ yes);
+        System.out.println("Inverse Yes Value: "+ !yes);
+    }
+}
