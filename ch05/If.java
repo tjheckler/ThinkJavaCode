b6f4cdb53178a2b1b57c80eda787a5c8622b95a8
tjheckler <timothyheckler@gmail.com>
2018-04-19 12:44:29 -0500

    5-es-1

---
+++ ch05/If.java
@@ -0,0 +1,13 @@
+public class If
+{
+    public static void main (String[] args)
+    {
+        if (5>1) System.out.println("Five is greater than one.");
+        if (2<4)
+        {
+            System.out.println("Two is less than four.");
+            System.out.println("Test succeeded.");
+        }
+
+    }
+}
