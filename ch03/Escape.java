444ad4eb609be062e366085365587ec8fd3e4f96
tjheckler <timothyheckler@gmail.com>
2018-04-18 21:56:26 -0500

    3-ES-1

---
+++ ch03/Escape.java
@@ -0,0 +1,15 @@
+public class Escape
+{
+    public static void main(String[] args)
+    {
+        String header = "\n\tNEWYORK 3-DAY FORCAST:\n";
+        header += "\n\tDay\t\tHigh\tLow\tConditions\n" ;
+        header += "\t---\t\t----\t---\t----------\n" ;
+
+        String forecast = "\tSunday\t\t68F\t48F\tSunny\n" ;
+        forecast += "\tMonday\t\t69F\t57F\tSunny\n" ;
+        forecast += "\tTuesday\t\t71F\t50F\tCloudy\n" ;
+
+        System.out.println(header + forecast);
+    }
+}
