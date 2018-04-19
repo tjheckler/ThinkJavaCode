09bd96b5722f337392a22ef162b11619c60cc403
tjheckler <timothyheckler@gmail.com>
2018-04-19 11:37:49 -0500

    5-es-1

---
+++ ch05/Precedence.java
@@ -0,0 +1,12 @@
+public class Precedence
+{
+    public static void main( String[] args)
+    {
+       int sum = 32 - 8 + 16 *2; //16x32 +, +24 = 56
+       System.out.println("Default order: "+ sum);
+       sum = (32-8+16)*2; //24+16=40,x2=80
+       System.out.println("Specified order: "+ sum);
+       sum =(32 - (8+16)) *2; //32-24=8, * 2 =16
+       System.out.println("Nested Specific order: "+ sum);
+    }
+}
