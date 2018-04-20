bc6b06602f4fd7de287a172c384b0117d4d39a0c
tjheckler <timothyheckler@gmail.com>
2018-04-19 19:26:40 -0500

    5 - E & D

--- ch05/SwitchExample.java
+++ ch05/SwitchExample.java
@@ -1,26 +1,51 @@
- class SwitchExample
+class SwitchExample
 {
     public static void main(String[] args)
     {
         lastNameWinner("Lazenby");
+        dayOfWeek(100);
     }
 
     public static void lastNameWinner(String lastName)
     {
-        switch (lastName )
+        switch (lastName)
         {
             case "smith":
-                System.out.println("Congratulations, grand winner");break;
+                System.out.println("Congratulations, grand winner");
+                break;
             case "jones":
-                System.out.println("Congratulations, grand winner");break;
+                System.out.println("Congratulations, grand winner");
+                break;
 
             case "Lazenby":
-                 System.out.println("Hey, he owes me dinner");break;
+                System.out.println("Hey, he owes me dinner");
+                break;
             default:
-                     System.out.println("Sorry, not a winner");
-
-
+                System.out.println("Sorry, not a winner");
         }
+
+    }
+
+    public static void dayOfWeek(int day)
+    {
+        switch (day)
+        {
+            case 1:
+                System.out.println("Sunday");break;
+            case 2:
+                System.out.println("Monday");break;
+            case 3:
+                System.out.println("Tuesday");break;
+            case 4:
+                System.out.println("Wednesday");break;
+            case 5:
+                System.out.println("Thursday");break;
+            case 6:
+                System.out.println("Friday");break;
+            case 7:
+                System.out.println("Saturday");break;
+        }
+        System.out.println("Invalid value: X");
     }
 }
 
