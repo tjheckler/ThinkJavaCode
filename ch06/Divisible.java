bc8c65ec4a1ad1e57831a2f46c1d2ff64ea4e2b5
tjheckler <timothyheckler@gmail.com>
2018-04-21 02:37:27 -0500

    6-2

---
+++ ch06/Divisible.java
@@ -0,0 +1,22 @@
+public class Divisible
+{
+
+public static void main(String[] args)
+{
+
+
+    System.out.println(isDivisible(10,2));
+
+}
+public static boolean isDivisible(int n, int m)
+{
+    if(n%m==0)
+    {
+        return true;
+    }
+    else
+        {
+    return false;
+        }
+}
+    }
