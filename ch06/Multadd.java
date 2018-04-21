460fb4c73403d5f0b93a48b16328297d935fadae
tjheckler <timothyheckler@gmail.com>
2018-04-21 03:31:26 -0500

    6-4, steps 1-3

---
+++ ch06/Multadd.java
@@ -0,0 +1,16 @@
+public class Multadd
+{
+    public static void main (String[] args)
+    {
+        System.out.println(Multadd(1,2,3));
+    }
+
+    public static double Multadd(double a, double b, double c)
+    {
+        double math = a*b +c;
+        return math;
+    }
+
+    //I started to look at step 4 and said to myself... not that smart
+    // ... couldn't attempt if it was required
+}
