0cb3db7572f1c00956b509d1e0d40d4c564be7bf
tjheckler <timothyheckler@gmail.com>
2018-04-23 14:43:06 -0500

    7-D

---
+++ ch07/For2.java
@@ -0,0 +1,16 @@
+public class For2
+{
+    public static void main(String[] args)
+    {
+        loopFor(10);
+    }
+    public static void loopFor(int n)
+    {
+
+        for (int i = 1 ; i <= n ; i++)
+        {
+            System.out.println(" Outer Loop i = " + i);
+
+        }
+    }
+}
