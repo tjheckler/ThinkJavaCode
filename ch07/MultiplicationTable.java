3b728168cea7434ae5c57f8a392554158cf69cb3
tjheckler <timothyheckler@gmail.com>
2018-04-23 20:48:10 -0500

    7-G

---
+++ ch07/MultiplicationTable.java
@@ -0,0 +1,32 @@
+public class MultiplicationTable
+{
+    public static void main(String[] args)
+    {
+
+        int tableSize = 5;
+
+        printMultiplicationTable(tableSize);
+
+    }
+
+
+    public static void printMultiplicationTable(int tableSize)
+    {
+
+        for (int i = 1; i <= tableSize; i++)
+        {
+
+            for (int j = 1; j <= tableSize; j++)
+            {
+
+                System.out.format("%4d\t", i * j);
+
+            }
+
+            System.out.println();
+
+        }
+
+
+    }
+}
