35c5eb7880076c0a2a828f65c450b2b55d3e86aa
tjheckler <timothyheckler@gmail.com>
2018-04-27 13:55:58 -0500

    8-H(silver)

--- ch08/IntergalacticVendingMachines.java
+++ ch08/IntergalacticVendingMachines.java
@@ -4,38 +4,47 @@ public class IntergalacticVendingMachines
 {
     public static void main(String[] args)
     {
-        String[] food = new String[3];
-        food[0] = "Freeze Dried Sushi";
-        food[1] = "Spock's Brain Blast";
-        food[2] = "Alien Asparagus";
-        printMenu(food);
+        Food driedSushi = new Food(" Freeze Dried Sushi",0);
+        Food brainBlast = new Food(" Spock's Brain Blast",1);
+       Food alienAsparagus = new Food(" Alien Asparagus",2);
 
+        Food[] meal = new Food[3];
+        meal[0] = driedSushi;
+        meal[1] = brainBlast;
+        meal[2] = alienAsparagus;
+
+        printMenu(meal);
         //count foods
-        int[] counter = new int[3];
-        counter[0] = 0;
-        counter[1] = 1;
-        counter[2] = 2;
+        //  int[] counter = new int[3];
+        // counter[0] = 0;
+        // counter[1] = 1;
+        // counter[2] = 2;
 
 
         //if select # 99
-        boolean chose;
 
 
     }
 
 
-    public static void printMenu(String[] food )
+    public static void printMenu(Food[] meals)
     {
+        int choice;
         Scanner in = new Scanner(System.in);
         //foods
+        System.out.println("Please Select an Item:" );
 
-        System.out.println("Please Select From: ");
-        System.out.println("0) " + food[0]);
-        System.out.println("1) " + food[1]);
-        System.out.println("2) " + food[2]);
+        //Prints Menu for selection
+for (Food meal: meals)
+{
+    System.out.println(meal.getId() +" "+meal.getName());
+}
         System.out.println();
-        int choice = in.nextInt();
-        System.out.println("Your Selection was " + food[choice]);
-    }
+        choice = in.nextInt();
+        System.out.println();
+        int selection = choice;
+        System.out.println("Your selection is: "+meals[selection].getName());
 
+
+    }
 }
