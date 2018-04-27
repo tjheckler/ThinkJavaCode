2b1367721d0519b63247a3aa19addb23e319f556
tjheckler <timothyheckler@gmail.com>
2018-04-27 15:13:21 -0500

    11-b-2

---
+++ ch11/DateTester.java
@@ -0,0 +1,14 @@
+public class DateTester
+{
+    public static void main(String[] args)
+    {
+        Date currentDate;
+
+         currentDate = new Date(27,04,2018);
+
+
+        System.out.println(currentDate.getformattedDate());
+    }
+
+
+}
