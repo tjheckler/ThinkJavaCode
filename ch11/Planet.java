9091e43c3210b5e57c3de5408f28cbb1fdf5874a
tjheckler <timothyheckler@gmail.com>
2018-04-27 15:13:39 -0500

    11-b-1

---
+++ ch11/Planet.java
@@ -0,0 +1,13 @@
+public class Planet
+{
+    private String name;
+    public Planet (String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
