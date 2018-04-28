3bfb7695b4551c6e02376d9dec07266a6ca9005e
tjheckler <timothyheckler@gmail.com>
2018-04-28 00:23:30 -0500

    8-H(silver)

--- ch08/IntergalacticVendingMachines.java
+++ ch08/IntergalacticVendingMachines.java
@@ -4,9 +4,9 @@ public class IntergalacticVendingMachines
 {
     public static void main(String[] args)
     {
-        Food driedSushi = new Food(" Freeze Dried Sushi",0);
-        Food brainBlast = new Food(" Spock's Brain Blast",1);
-       Food alienAsparagus = new Food(" Alien Asparagus",2);
+        Food driedSushi = new Food(" Freeze Dried Sushi", 1);
+        Food brainBlast = new Food(" Spock's Brain Blast", 2);
+        Food alienAsparagus = new Food(" Alien Asparagus", 3);
 
         Food[] meal = new Food[3];
         meal[0] = driedSushi;
@@ -14,11 +14,11 @@ public class IntergalacticVendingMachines
         meal[2] = alienAsparagus;
 
         printMenu(meal);
-        //count foods
-        //  int[] counter = new int[3];
-        // counter[0] = 0;
-        // counter[1] = 1;
-        // counter[2] = 2;
+
+         int[] counter = new int[3];
+         counter[0] = 0;
+         counter[1] = 1;
+         counter[2] = 2;
 
 
         //if select # 99
@@ -32,19 +32,39 @@ public class IntergalacticVendingMachines
         int choice;
         Scanner in = new Scanner(System.in);
         //foods
-        System.out.println("Please Select an Item:" );
+        System.out.println("Please Select an Item:");
 
         //Prints Menu for selection
-for (Food meal: meals)
-{
-    System.out.println(meal.getId() +" "+meal.getName());
-}
+        for (Food meal : meals)
+        {
+            System.out.println(meal.getId() + ") " + meal.getName());
+        }
         System.out.println();
         choice = in.nextInt();
         System.out.println();
-        int selection = choice;
-        System.out.println("Your selection is: "+meals[selection].getName());
 
+        /*------------------------------------------------------------------*/
+
+        int selection = choice;
+
+        if (choice < 4)
+        {
+            System.out.println("Thank you for choosing: " + meals[selection].getName());
+        }
+
+        if (choice >= 4 && choice < 99 || choice >99)
+        {
+            System.out.println("Invalid Selection please try again");
+
+        }
+        if (choice == 99)
+        {
+            System.out.println("Counter goes here");
+
+        }
+
+        /*------------------------------------------------------------------*/
 
     }
+
 }
