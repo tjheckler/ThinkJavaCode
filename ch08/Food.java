2559588edeb6219dd838a43d370bfc652cd436e4
tjheckler <timothyheckler@gmail.com>
2018-04-28 00:23:17 -0500

    8-H(silver)

---
+++ ch08/Food.java
@@ -0,0 +1,26 @@
+public class Food
+{
+
+    private String name;
+    private int id;
+
+
+    public Food (String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+
+}
